988eafd12d495b8e967f2ae5f4a44af26758739d
imrabti <imrabti@gmail.com>
2015-09-13 18:10:59 +0100

    Added new .gitignore file.

--- src/main/java/org/nuvola/myapp/server/spring/SsoConfig.java
+++ src/main/java/org/nuvola/myapp/server/spring/SsoConfig.java
@@ -2,23 +2,8 @@ package org.nuvola.myapp.server.spring;
 
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableOAuth2Sso
-public class SsoConfig extends WebSecurityConfigurerAdapter {
-    @Override
-    public void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
-                .antMatcher("/app/**")
-                .authorizeRequests()
-                .anyRequest()
-                .authenticated()
-                .and()
-                .logout()
-                .logoutUrl("/logout")
-                .permitAll()
-                .logoutSuccessUrl("/");
-    }
+public class SsoConfig {
 }
