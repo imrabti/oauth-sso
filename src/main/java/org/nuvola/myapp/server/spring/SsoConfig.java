3efc58adf8cdde0f1616ad35122a3661d993bf0c
imrabti <imrabti@gmail.com>
2015-09-15 07:51:44 +0100

    Adjusted the application so that the index.html is the GWT hosting page.

--- src/main/java/org/nuvola/myapp/server/spring/SsoConfig.java
+++ src/main/java/org/nuvola/myapp/server/spring/SsoConfig.java
@@ -2,6 +2,7 @@ package org.nuvola.myapp.server.spring;
 
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
